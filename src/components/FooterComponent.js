9176be11882e7c1c00b9aeaa988de7a4594f952d
NuwanJ <nuwanjaliyagoda@gmail.com>
2020-11-23 21:42:16 +0530

    code formatting

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -6,15 +6,21 @@ function Footer() {
         <div className="footer">
             <div className="container">
                 <div className="row justify-content-left">
-                        <div className="text-center">
-                            <span><FaGithub size={25} /></span>
-                            <span>&nbsp;</span>
-                            <span><a href="http://google.com/+"><u>https://github.com/Pera-Swarm</u></a></span>
+                    <div className="text-center">
+                        <span>
+                            <FaGithub size={25} />
+                        </span>
+                        <span>&nbsp;</span>
+                        <span>
+                            <a href="http://google.com/+">
+                                <u>https://github.com/Pera-Swarm</u>
+                            </a>
+                        </span>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Footer;
