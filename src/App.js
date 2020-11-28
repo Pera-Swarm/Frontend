a1af5f20a0f3de46ccebed8edec1b291506c14a8
NuwanJ <nuwanjaliyagoda@gmail.com>
2020-11-28 21:58:28 +0530

    Added MQTT support

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React, { Component } from 'react';
+import React,{ Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import mqttConfig from './config/mqttConfig';
 
-class App extends Component {
-    render() {
+import MQTT from 'paho-mqtt';
+
+class App extends Component{
+
+    constructor(props){
+        super(props)
+
+        //console.log(mqttConfig);
+
+        var client = new MQTT.Client(mqttConfig.host, Number(mqttConfig.port), "/socket.io",mqttConfig.options.clientId);
+
+        //if(client.connect !== undefined){
+        client.connect({
+            userName: "swarm_user",
+            password: "swarm_usere15",
+            reconnect: true,
+            useSSL: true,
+            cleanSession : false,
+            onSuccess: () => {
+                client.subscribe("test");
+                console.log('MQTT: connected');
+
+                //onMessageArrived = onMessageArrived;
+                //onConnectionLost = onConnectionLost
+
+            },
+            onFailure: ()=>{
+                console.log('MQTT: connection failed');
+            }
+
+        });
+    }
+
+    render(){
         return (
-            <BrowserRouter>
+            <BrowserRouter basename={'/swarm-admin'}>
                 <div>
                     <Main />
                 </div>
---
+++ src/config/mqttConfig.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+
+export default {
+
+    host: process.env.REACT_APP_MQTT_HOST || 'localhost',
+    port: process.env.REACT_APP_MQTT_PORT || 8883,
+    path: process.env.REACT_APP_MQTT_PATH || '/',
+    options: {
+        port: 8883,
+        clientId: process.env.REACT_APP_MQTT_CLIENT,
+        clientId: 'admin_' + Math.random().toString(16).substr(2, 8),
+        username: process.env.REACT_APP_MQTT_USER || '',
+        password: process.env.REACT_APP_MQTT_PASS || ''
+    },
+    mqttOptions: {
+        qos: 2,
+        rap: false,
+        rh: true
+    }
+};
